e24a3d88f68130b4e371c3c6ea7a447026ceea88
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-08-26 01:29:38 +0900

    Add keyboard_press_callback

--- keyboard_wiretap.cpp
+++ keyboard_wiretap.cpp
@@ -1,7 +1,13 @@
 #include "keyboard_wiretap.h"
 
+void default_keyboard_press_callback()
+{
+  // デフォルトでは何もしない．
+}
+
 KeyboardWiretap::KeyboardWiretap() : hid_keyboard(&usb)
 {
+  keyboard_press_callback_ptr = default_keyboard_press_callback;
 }
 
 KeyboardWiretap::~KeyboardWiretap()
@@ -12,6 +18,13 @@ void KeyboardWiretap::begin()
 {
 }
 
-void KeyboardWiretap::set_keyboard_press_callback()
+void KeyboardWiretap::begin(void (*keyboard_press_callback_ptr)())
 {
+  set_keyboard_press_callback(keyboard_press_callback_ptr);
+  begin();
+}
+
+void KeyboardWiretap::set_keyboard_press_callback(void (*keyboard_press_callback_ptr)())
+{
+  this->keyboard_press_callback_ptr = keyboard_press_callback_ptr;
 }
--- keyboard_wiretap.h
+++ keyboard_wiretap.h
@@ -11,12 +11,14 @@ class KeyboardWiretap {
   HIDBoot<USB_HID_PROTOCOL_KEYBOARD> hid_keyboard;
   BleKeyboard ble_keyboard;
   Ticker ticker;
+  void (*keyboard_press_callback_ptr)();
 
  public:
   KeyboardWiretap();
   ~KeyboardWiretap();
   void begin();
-  void set_keyboard_press_callback();
+  void begin(void (*keyboard_press_callback_ptr)());
+  void set_keyboard_press_callback(void (*keyboard_press_callback_ptr)());
 };
 
 #endif // KEYBOARD_WIRETAP_H_
