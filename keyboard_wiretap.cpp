32cef46427b42b9eadc9ffc290d3e14e51449d37
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-08-26 01:14:53 +0900

    Add member var and func

--- keyboard_wiretap.cpp
+++ keyboard_wiretap.cpp
@@ -7,3 +7,11 @@ KeyboardWiretap::KeyboardWiretap() : hid_keyboard(&usb)
 KeyboardWiretap::~KeyboardWiretap()
 {
 }
+
+void KeyboardWiretap::begin()
+{
+}
+
+void KeyboardWiretap::set_keyboard_press_callback()
+{
+}
--- keyboard_wiretap.h
+++ keyboard_wiretap.h
@@ -1,13 +1,17 @@
 #include <hidboot.h>     // USBおよびHIDBootを使うためにインクルード
 #include <BleKeyboard.h> // BleKeyboardを使うためにインクルード
+#include <Ticker.h>
 
 class KeyboardWiretap {
  private:
   USB usb;
   HIDBoot<USB_HID_PROTOCOL_KEYBOARD> hid_keyboard;
   BleKeyboard ble_keyboard;
+  Ticker ticker;
 
  public:
   KeyboardWiretap();
   ~KeyboardWiretap();
+  void begin();
+  void set_keyboard_press_callback();
 };
