4fcd23a401a8e1176527c82acffc1dedcde63d31
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-08-28 02:21:28 +0900

    Fix hid_usage_id_to_key_code function

--- keyboard_wiretap.cpp
+++ keyboard_wiretap.cpp
@@ -85,7 +85,7 @@ void KeyboardWiretap::timer_callback()
 char KeyboardWiretap::hid_usage_id_to_key_code(char hid_usage_id)
 {
   if (4 <= hid_usage_id && hid_usage_id <= 29) {
-    return static_cast<char>(hid_usage_id - 4 + 'A');
+    return static_cast<char>(hid_usage_id - 4 + 'a');
   }
   return 0;
 }
