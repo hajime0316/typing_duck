4726a6bcb7caaa707759071176b34099256a889b
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-08-29 00:48:25 +0900

    Use hid_usage_id_to_key_code()

--- keyboard_wiretap.cpp
+++ keyboard_wiretap.cpp
@@ -19,12 +19,12 @@ static void default_keyboard_press_callback()
 void KeyboardWiretap::OnKeyDown(uint8_t mod, uint8_t key)
 {
   keyboard_press_callback_ptr_();
-  ble_keyboard_.press(OemToAscii(0, key));
+  ble_keyboard_.press(hid_usage_id_to_key_code(key));
 }
 
 void KeyboardWiretap::OnKeyUp(uint8_t mod, uint8_t key)
 {
-  ble_keyboard_.release(OemToAscii(0, key));
+  ble_keyboard_.release(hid_usage_id_to_key_code(key));
 }
 
 void KeyboardWiretap::OnControlKeysChanged(uint8_t before, uint8_t after)
