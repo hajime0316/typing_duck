80160c5e5803db16257134f5f759d908446a83f7
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-08-30 02:00:18 +0900

    Add growth_status.cpp/h

---
+++ growth_status.cpp
@@ -0,0 +1,16 @@
+#include "growth_status.h"
+
+GrowthStatus::GrowthStatus()
+{
+  level_status = 0;
+  evolution_status = 0;
+  generation_status = 0;
+}
+
+GrowthStatus::~GrowthStatus()
+{
+}
+
+GrowthStatus::begin()
+{
+}
---
+++ growth_status.h
@@ -0,0 +1,18 @@
+#ifndef GROWTH_STATUS_H_
+#define GROWTH_STATUS_H_
+
+#include <Ticker.h>
+
+class GrowthStatus {
+ private:
+  int level_status;      // レベルを表す
+  int evolution_status;  // 進化の段階を表す．ダックは0で進化するごとに1増える
+  int generation_status; // 世代を表す
+
+ public:
+  GrowthStatus();
+  ~GrowthStatus();
+  void begin();
+};
+
+#endif // GROWTH_STATUS_H_
