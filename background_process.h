40fb779833ad7cf1c9768cdf6e8438cbac2354b5
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-08-29 19:53:29 +0900

    Add status variables

--- background_process.h
+++ background_process.h
@@ -3,10 +3,23 @@
 
 #include <Ticker.h>
 
+enum class TypingStatus {
+  WAITING,         // 待機中
+  TYPING,         // タイピング中
+  PROMPTING_REST, // 休憩促し
+  REJECTING_INPUT // 入力拒否
+};
+
 class BackgroundProcess {
  private:
   Ticker ticker_;
   time_t present_time; // 電源を入れてから現在までの時刻[100ms]
+
+  // ステータス
+  TypingStatus typing_status;
+  int level_status;
+  int generation_status;
+  int evolution_status;
 
  public:
   BackgroundProcess();
