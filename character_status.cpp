bb34f6cd5f87a3837458eedc5630c314a89081df
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-08-30 17:48:14 +0900

    Update statuses

--- character_status.cpp
+++ character_status.cpp
@@ -3,6 +3,8 @@
 // CharacterStatusクラスをインスタンス化
 CharacterStatus character_status;
 
+static const double EXP_WEIGHT = 1.0 / 50.0;
+
 CharacterStatus::CharacterStatus()
 {
   level_status = 0;
@@ -21,4 +23,17 @@ void CharacterStatus::begin()
 
 void CharacterStatus::update()
 {
+  int exp = background_process.get_exp();
+
+  level_status = static_cast<int>(exp * EXP_WEIGHT) % 100 + 1;
+  if (1 <= level_status && level_status < 20) {
+    evolution_status = 0;
+  }
+  else if (20 <= level_status && level_status < 50) {
+    evolution_status = 1;
+  }
+  else {
+    evolution_status = 2;
+  }
+  generation_status = static_cast<int>(exp * EXP_WEIGHT) / 100 + 1;
 }
